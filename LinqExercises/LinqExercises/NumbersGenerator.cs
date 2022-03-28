3ccd3fd7ca25af1c2727790d5d8f0d15dfac2dcb
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-03-28 23:06:43 +0300

    start linq part 1

---
+++ LinqExercises/LinqExercises/NumbersGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LinqExercises
+{
+    public static class NumbersGenerator
+    {
+        public static IEnumerable<int> Generate()
+        {
+            int start = 0;
+            while (true)
+            {
+                // Console.WriteLine($"Infinite generator: {start}");
+                yield return start;
+                if (start < int.MaxValue)
+                {
+                    start++;
+                }
+                else
+                {
+                    start = 0;
+                }
+            }
+        }
+    }
+}
---
+++ LinqExercises/LinqExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+
+            /*
+            var query = from nr in NumbersGenerator.Generate()
+                        where nr % 2 == 0
+                        select nr;
+            */
+
+            var query = NumbersGenerator.Generate()
+                .Where(nr => nr % 2 == 0);
+
+            int i = 0;
+            foreach (int nr in query)
+            {
+                i++;
+                Console.WriteLine(nr);
+                if (i >= n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
